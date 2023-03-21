c1d5aa4a6005f244a272db8e0bfa2391278879ac
lwj <395901976@qq.com>
2023-03-21 10:43:59 +0800

    去除注释

--- app/src/main/java/com/captain/testbarchart/MainActivity.java
+++ app/src/main/java/com/captain/testbarchart/MainActivity.java
@@ -11,6 +11,7 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
     CaptainBarChartView cvTest;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
